5b901c7038f68b8093df353dd333004ba340362a
Nejat Boy <nejatboy34@gmail.com>
2021-11-19 20:16:00 +0300

    Project created.

---
+++ MoviesApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Nejat BOY on 19.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .systemRed
+    }
+
+
+}
+
